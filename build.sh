452526e8e0d6f1e025aa91fba37b5e61302282a1
yabuki829 <syodai829@icloud.com>
2024-02-02 23:58:47 +0900

    fix

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrations api
+python manage.py makemigrations api
 python manage.py migrate
 python manage.py superuser
